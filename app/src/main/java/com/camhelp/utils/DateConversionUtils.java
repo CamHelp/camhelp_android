acac058eab76013312e7be32520a040229fae468
stonehell <2499144744@qq.com>
2017-09-12 20:05:37 +0800

    2017-09-12

--- app/src/main/java/com/camhelp/utils/DateConversionUtils.java
+++ app/src/main/java/com/camhelp/utils/DateConversionUtils.java
@@ -13,25 +13,27 @@ import java.util.Date;
 public class DateConversionUtils {
 
     /*判断日期和当前是否在同一天*/
-    public static boolean isToday(String stime){
+    public static boolean isToday(String stime) {
         Calendar calendar = Calendar.getInstance();
         long now = Long.parseLong(stime);
         calendar.setTimeInMillis(now);
-        SimpleDateFormat fmt=new SimpleDateFormat("yyyy-MM-dd");
-        if(fmt.format(calendar.getTime()).toString().equals(fmt.format(new Date()).toString())){//格式化为相同格式
+        SimpleDateFormat fmt = new SimpleDateFormat("yyyy-MM-dd");
+        if (fmt.format(calendar.getTime()).toString().equals(fmt.format(new Date()).toString())) {//格式化为相同格式
             return true;
-        }else {
+        } else {
             return false;
         }
-    }/*判断日期和当前是否在同一年*/
-    public static boolean isThisYear(String stime){
+    }
+
+    /*判断日期和当前是否在同一年*/
+    public static boolean isThisYear(String stime) {
         Calendar calendar = Calendar.getInstance();
         long now = Long.parseLong(stime);
         calendar.setTimeInMillis(now);
-        SimpleDateFormat fmt=new SimpleDateFormat("yyyy");
-        if(fmt.format(calendar.getTime()).toString().equals(fmt.format(new Date()).toString())){//格式化为相同格式
+        SimpleDateFormat fmt = new SimpleDateFormat("yyyy");
+        if (fmt.format(calendar.getTime()).toString().equals(fmt.format(new Date()).toString())) {//格式化为相同格式
             return true;
-        }else {
+        } else {
             return false;
         }
     }
@@ -54,18 +56,18 @@ public class DateConversionUtils {
         long ltime = Long.parseLong(stime);
         double fromNowMin = (nowTime.getTime() - ltime) / 60000.0; //距离现在分钟数
 
-        if (fromNowMin < 1) {//距现在时间少于1分钟
+        if (fromNowMin < 1) {                       //距现在时间少于1分钟
             formatTime = "刚刚";
-        } else if (fromNowMin < 10) {//距现在时间少于10分钟
+        } else if (fromNowMin < 10) {               //距现在时间少于10分钟
             formatTime = "" + (int) fromNowMin + "分钟前";
-        } else if (fromNowMin < 60) {//距现在时间少于60分钟
-            formatTime = "" + (int) fromNowMin/10 + "0分钟前";
-        }else if (isToday(stime)) {//在同一天，分
+        } else if (fromNowMin < 60) {               //距现在时间少于60分钟
+            formatTime = "" + (int) fromNowMin / 10 + "0分钟前";
+        } else if (isToday(stime)) {                //在同一天，分
             Calendar calendar = Calendar.getInstance();
             long now = Long.parseLong(stime);
             calendar.setTimeInMillis(now);
             formatTime = formatter2.format(calendar.getTime());
-        }else if (isThisYear(stime)) {//在同一年，月日分
+        } else if (isThisYear(stime)) {             //在同一年，月日分
             Calendar calendar = Calendar.getInstance();
             long now = Long.parseLong(stime);
             calendar.setTimeInMillis(now);
